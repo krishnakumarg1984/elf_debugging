9b155c94314e5c6625da736f8cef92fa4319359a
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2024-11-08 11:24:34 +0000

    updates to 00_segfault1

--- 00_segfault1.c
+++ 00_segfault1.c
@@ -1,18 +1,39 @@
 #include <stdio.h>
+#include <sys/ptrace.h>
 #include <unistd.h>
 
 int main()
 {
+    /*
+    if (ptrace(PTRACE_TRACEME, 0, NULL, 0) == -1)
+    {
+        printf("%s", "I am a debugee!\n");
+    }
+    */
+
+    /*
+    int x = 0;
+    int* p = &x;
+    */
+
+    int* p = 0;
+
     printf("Hello UCL-ARC!\n");
     sleep(1);
     printf("Let us talk about an important HPC topic\n");
     sleep(1);
     printf("*Hint* it's about debugging\n");
     sleep(1);
-    /* int x = 0; */
-    /* int* p = &x; */
-    int* p = NULL;
-    *p     = 7;
+
+    /* a few other
+       lines and
+       blocks of code */
+
+    /* more such
+       lines and
+       blocks of code */
+
+    *p = 7;
 
     return 0;
 }
